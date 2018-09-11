ecb92420275021ffa7874143e565b76329ed1794
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2018-09-10 17:28:43 -0700

    added header

--- Classwork/Classwork01/SayHello.java
+++ Classwork/Classwork01/SayHello.java
@@ -1,3 +1,13 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  SayHello.java
+ *  Purpose       :  To make sure Java and GitHub environemnt is working correctly. 
+ *  Author        :  Talia Bahar
+ *  Date          :  2018-08-30
+ *  Description   :  Prompts user to input their name and then says Hello to them.
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 import java.util.Scanner;
 
 public class SayHello {
